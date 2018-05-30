9b68cde95f70f62135850f10d991d1046800dd8a
Nischay <nischay@Nischays-MacBook-Pro.local>
2018-05-30 06:20:18 +0530

    mongoclient option true

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const mongoURL = process.env.MONGO_URL || 'mongodb://localhost/tasker'
 
-mongoose.connect(mongoURL, function(err) {
+mongoose.connect(mongoURL, {useMongoClient: true},function(err) {
   if(err) { process.exit(-1); }
   console.log('Connected to MongoDB');
 });
